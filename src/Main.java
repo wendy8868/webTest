ec1baabaa9533823b4edff38cd8a7fc90f93c9fb
wendy8868 <2915777@qq.com>
2022-10-09 23:00:16 +0800

    测试提交代码！

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("llf是sb");
-        System.out.println("qlf是帅逼");
+        System.out.println("qlf是SB");
+
+        //我写了一个测试代码，并打算提交
+        System.out.println("哈哈！");
     }
 }
