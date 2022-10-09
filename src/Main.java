ece6c8f6f84e3aadad4244a9568b5cf0128ac924
HyrKG <end_kg@126.com>
2022-10-09 23:02:39 +0800

    删除所有代码

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("llf是sb");
-        System.out.println("qlf是SB");
 
-        //我写了一个测试代码，并打算提交
-        System.out.println("哈哈！");
-        //test
-        System.out.println("QLF大沙北");
     }
 }
